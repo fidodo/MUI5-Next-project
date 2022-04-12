ab40e361c2967f526fadb5e8170df8b5ad789f99
Ayokunle Ogunfidodo <ayokunleogunfidodo@gmail.com>
2022-04-12 21:22:01 +0300

    webpack updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c4f149edd7dabdd"; }
+/******/ 		__webpack_require__.h = function() { return "32e21d509cc52552"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
